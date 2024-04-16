5d971b1315d4f762a6ecf0a746ceabf2d6b700ed
Nenad Bozinovic <nesaboz@gmail.com>
2024-04-16 00:49:28 -0700

    plotted xy of a ball

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,8 +2,11 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
-# print(result)
-# print("boxes:")
-# for box in result[0].boxes:
-#     print(box)
+result = model.predict('input_videos/short.mov', save=True)
+
+
+# result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
